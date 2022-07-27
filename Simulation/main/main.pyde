8672d9bb3a93ffe9291b168921bb1d3e1be72064
KaiduY <alex.oprea2003@gmail.com>
2022-07-27 15:25:23 +0300

    Finalized the model

--- Simulation/main/main.pyde
+++ Simulation/main/main.pyde
@@ -1,16 +1,12 @@
-from arm import arm
-from joint import joint
+from model import model
 
-b1 = arm(50)
-b2 = arm(50)
-b3 = arm(100)
-b4 = arm(200)
-j1 = joint(b1,b2)
-j2 = joint(b2,b3)
-j3 = joint(b3,b4)
+model = model(50)
 
 def setup():
+    global model
     size(512, 512, P3D)
+    model.setStartingPossition(100, 100, 0)
+    model.setStartingPossitionJoints(30, 20, 20, 20)
     #print(b1.lenght)
     
 i = 0
@@ -19,17 +15,7 @@ def draw():
     global i
     background(220)
     lights()
-    #translate(100, 100, 0)
-    translate (200, 200, 0)
-    rotateY(radians(i))
-    b1.pos(0,0,0,0,0)
-    j1.rot(20)
-    j2.rot(90)
-    j3.rot(-20)
-    #rotateX(radians(i))
-    #b1.update()
-    #b2.update()
-    i=i+1
+
     
 
     
--- Simulation/main/model.py
+++ Simulation/main/model.py
@@ -11,18 +11,40 @@ class model:
         self.j1 = joint(self.b1, self.b2)
         self.j2 = joint(self.b2, self.b3)
         self.j3 = joint(self.b3, self.b4)
-        self.start = (0,0,0,0)
+        self.start = (0,0,0)
+        self.startj = (0,0,0,0)
         self.mv = (0,0,0,0,0)
     
-    def setStartingPossition(self, x, y, z, a=0):
-        self.start = (x, y, z, a)
+    def setStartingPossition(self, x, y, z):
+        self.start = (x, y, z)
+    
+    def setStartingPossitionJoints(self, s0, s1, s2, s3):
+        self.startj = (s0, s1, s2, s3)
+        self._init()
         
     def move(self, s0, s1, s2, s3):
         self.mv = (s0, s1, s2, s3)
+        self.update()
         
-    def update():
-        x,y,z,_ = self.start
+    def _init(self):
+        x,y,z = self.start
+        s0, s1, s2, s3 = self.startj
         translate(x,y,z)
+        rotateY(s0)
+        self.b1.pos(0,0,0,0,0)
+        self.j1.rot(s1)
+        self.j2.rot(s2)
+        self.j3.rot(s3)
+        
+    def update(self):
+        x,y,z = self.start
+        s0, s1, s2, s3 = self.mv
+        translate(x,y,z)
+        rotateY(s0)
+        self.b1.pos(0,0,0,0,0)
+        self.j1.rot(s1)
+        self.j2.rot(s2)
+        self.j3.rot(s3)
         
         
         
